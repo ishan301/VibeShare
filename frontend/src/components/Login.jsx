3295dee1b3fca1ab3b4f292a955487e26e5e736f
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-04-07 23:39:16 +0530

    added assets

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import shareVideo from '../assets/share.mp4';
+
+const Login = () => {
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className='relative w-full h-full'>
+        <video
+        src={shareVideo}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+        autoPlay
+        className='w-full h-full object-cover'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Login
