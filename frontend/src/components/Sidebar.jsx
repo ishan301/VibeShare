f471985a392275a75b8c53d55af6ee970f84a0ec
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-04-11 23:28:35 +0530

    added sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import {RiHomeFill} from 'react-icons/ri';
+import {IoIosArrowForward} from 'react-icons/io';
+import logo from '../assets/logo.png'
 
 const Sidebar = () => {
   return (
